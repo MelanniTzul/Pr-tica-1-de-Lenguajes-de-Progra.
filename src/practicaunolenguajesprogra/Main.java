60f6a973082355629be6089c85f1862328e0308e
Melanni Tzul <melannitzul@gmail.com>
2021-08-04 22:40:30 -0600

    Cree una ventana y le añadi fondo

--- src/practicaunolenguajesprogra/Main.java
+++ src/practicaunolenguajesprogra/Main.java
@@ -5,13 +5,16 @@ package practicaunolenguajesprogra;
  *
  * @author Melanni Tzul
  */
-public class PracticaUnoLenguajesProgra {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Menu ventana = new  Menu();
+        ventana.setVisible(true);
+        
+      
     }
     
 }
