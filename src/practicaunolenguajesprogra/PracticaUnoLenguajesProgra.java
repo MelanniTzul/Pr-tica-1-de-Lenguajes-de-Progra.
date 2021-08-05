d70c2ba9fa5ff9ab23a516abdf3134524a42c3ba
Melanni Tzul <melannitzul@gmail.com>
2021-08-04 21:51:23 -0600

    Cree el practica uno de lenguajes de programación.

---
+++ src/practicaunolenguajesprogra/PracticaUnoLenguajesProgra.java
@@ -0,0 +1,17 @@
+
+package practicaunolenguajesprogra;
+
+/**
+ *
+ * @author Melanni Tzul
+ */
+public class PracticaUnoLenguajesProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
